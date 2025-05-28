c962aeca3cf7329f6be2585212a00ec4d9760e9f
LucasDebeve <lucas.debeve2003@gmail.com>
2025-05-28 12:19:22 +0200

    fix: header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,6 @@
 function Header() {
     return (
-        <section id="header" className="grid grid-cols-1 md:grid-cols-2 max-h-[90svh] border-b border-zinc-300 dark:border-zinc-600 dark:text-gray-300">
+        <section id="header" className="grid grid-cols-1 md:grid-cols-2 md:max-h-[90svh] border-b border-zinc-300 dark:border-zinc-600 dark:text-gray-300">
             <div className="min-h-[50svh] md:min-h-auto flex flex-col justify-center items-center p-4 border-r border-zinc-300 dark:border-zinc-600">
                 <h1 className="text-7xl md:text-9xl font-display text-center">Lucas <br /> Debeve</h1>
                 <p className="text-xl md:text-2xl font-light text-center mt-4">Président de la République Française</p>
